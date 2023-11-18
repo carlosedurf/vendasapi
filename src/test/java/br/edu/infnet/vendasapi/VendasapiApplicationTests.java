5e035258577d34c0d7558f74e55784143c7da2b2
Carlos Eduardo <carloseduardo.rocha.f@gmail.com>
2023-11-18 10:23:34 -0300

    Inicio do projeto

---
+++ src/test/java/br/edu/infnet/vendasapi/VendasapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.vendasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
